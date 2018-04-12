f75a13dcbac6fd7945608858e3cfc9b80957fa28
Lucas Vieira <lucasvieira0073@hotmail.com>
2018-04-12 20:24:35 -0300

    alterações 1

--- app/src/main/java/br/com/lucas/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/lucas/agenda/ListaAlunosActivity.java
@@ -2,6 +2,9 @@ package br.com.lucas.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -11,8 +14,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_alunos);
 
         String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);//alt + enter faz o import
 
-        findViewById(R.id.lista_alunos);
+        //convertendo Strings em Views
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, alunos);
+
+        //vincula a lista com o adapter
+        listaAlunos.setAdapter(adapter);
+
 
 
 
